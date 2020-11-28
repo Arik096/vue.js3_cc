063a514c7213548e5c501b2d4c9270784e9473c2
Arik Md. Isthiaque <arikbncc@gmail.com>
2020-11-28 19:01:27 +0600

    Update App.vue

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+
+<div id="app">
+
+    @{{user.username}} - {{fullname}}
+    <br>
+    <strong>Followers:</strong> {{followers}}
+    <br>
+    <button @click="followUser">
+        Follow
+    </button>
+
+    <button @click="ResetfollowUser">
+        Reset
+    </button>
+
+</div>
+
+</template>
+
+
+<script>
+
+export default {
+    name: 'App',
+    data(){
+        return{
+            followers: 0,
+            user:{
+                id: 1,
+                username: '_arik5001',
+                firstname: 'Arik',
+                lastname: 'Md Isthiaque',
+                email: 'arik@isocial.com.bd',
+                isAdmin: true
+            }
+        }
+    },
+    watch:{
+        followers(newfollowersCount, oldfollowersCount){
+            if(oldfollowersCount<newfollowersCount){
+                console.log(this.user.username+' has gained a follower!')
+            }
+        }
+    },
+    computed:{
+        fullname(){
+            return  this.user.firstname+' '+this.user.lastname
+        }
+    },
+    methods:{
+        followUser() {
+            this.followers++
+        },
+        ResetfollowUser(){
+            this.followers= 0
+        }
+    },
+    mounted(){
+        this.followUser();
+    }
+}
+
+</script>
+
+<style>
+
+    #app {
+    font-family: consolas;
+    text-align: center;
+    color: info;
+    margin-top: 10px;
+    }
+    
+</style>
